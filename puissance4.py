b94c46c64934ebc20ed82e110ed9c5147742187d
Thibaut Chaillet <thibaut.chaillet@gmail.com>
2025-03-04 12:50:30 +0100

    Update puissance4.py

--- puissance4.py
+++ puissance4.py
@@ -4,42 +4,40 @@ import random as rd
 racine = tk.Tk()
 racine.title("Puissance 4")
 
+liste=[]
+
 def placer_jeton(x):
-    global cancel
-    cancel=x
+    global liste
+    liste.append(x)
     print(x)
 
-#def creer_grille():
-#    mon_canvas.create_rectangle(500,500,500,500,fill="white")
 
-bouton_1 = tk.Button(racine,text="1",command=placer_jeton(1))
-bouton_2 = tk.Button(racine,text="2",command=placer_jeton(2))
-bouton_3 = tk.Button(racine,text="3",command=placer_jeton(3))
-bouton_4 = tk.Button(racine,text="4",command=placer_jeton(4))
-bouton_5 = tk.Button(racine,text="5",command=placer_jeton(5))
-bouton_6 = tk.Button(racine,text="6",command=placer_jeton(6))
-bouton_7 = tk.Button(racine,text="7",command=placer_jeton(7))
+bouton_1 = tk.Button(racine,text="1",command=placer_jeton(1),font=("helvetica", "30"))
+bouton_2 = tk.Button(racine,text="2",command=placer_jeton(2),font=("helvetica", "30"))
+bouton_3 = tk.Button(racine,text="3",command=placer_jeton(3),font=("helvetica", "30"))
+bouton_4 = tk.Button(racine,text="4",command=placer_jeton(4),font=("helvetica", "30"))
+bouton_5 = tk.Button(racine,text="5",command=placer_jeton(5),font=("helvetica", "30"))
+bouton_6 = tk.Button(racine,text="6",command=placer_jeton(6),font=("helvetica", "30"))
+bouton_7 = tk.Button(racine,text="7",command=placer_jeton(7),font=("helvetica", "30"))
 
 
-CANVAS_WIDTH = 900
-CANVAS_HEIGHT = 775
+CANVAS_WIDTH = 700
+CANVAS_HEIGHT = 600
 mon_canvas = tk.Canvas(racine, background="blue", width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
-#creer_grille()
 
-bouton_1.grid(row=0,column=1)
-bouton_2.grid(row=0,column=2)
-bouton_3.grid(row=0,column=3)
-bouton_4.grid(row=0,column=4)
-bouton_5.grid(row=0,column=5)
-bouton_6.grid(row=0,column=6)
-bouton_7.grid(row=0,column=7)
-mon_canvas.grid(row=1, column=1, rowspan=6 , columnspan=7)
+largeur_case = CANVAS_WIDTH//7
+hauteur_case = CANVAS_HEIGHT//6
+for i in range(7):
+    for j in range(6):
+        mon_canvas.create_oval((25+i*largeur_case, 25+j*hauteur_case),((i+1)*largeur_case, (j+1)*hauteur_case), fill="white")
+
+bouton_1.grid(row=0,column=2)
+bouton_2.grid(row=0,column=3)
+bouton_3.grid(row=0,column=4)
+bouton_4.grid(row=0,column=5)
+bouton_5.grid(row=0,column=6)
+bouton_6.grid(row=0,column=7)
+bouton_7.grid(row=0,column=8)
+mon_canvas.grid(row=1, column=2, rowspan=6 , columnspan=7)
 
 racine.mainloop()
-
-#test
-largeur_case = 100
-hauteur_case = 100
-for i in range(8):
-    for j in range(8):
-        canvas.create_rectangle((i*largeur_case, j*hauteur_case),((i+1)*largeur_case, (j+1)*hauteur_case), fill=color)
